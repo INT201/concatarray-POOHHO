987ca9c2041c4ca76c567722b37cdd89977a8c2d
POOHHO <patcharaphol.pooh@mail.kmutt.ac.th>
2022-09-14 23:53:26 +0700

    POOH

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,16 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  let a = array1
+  let b = array2
+  if((a == null || a == undefined)&&(b == null || b== undefined)){
+    return undefined
+  }else if(a == null || a == undefined){
+    return b
+  }else if(b == null || b == undefined){
+    return a
+  }else{
+    return  a.concat(b)
+  }
 }
 module.exports = concatArray
